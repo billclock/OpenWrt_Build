b95dc1246bda1b304b34705eab2b81c9916d8764
billzhong <billzhong@tencent.com>
2023-04-18 11:21:14 +0800

    remove unnecessary files

--- diy/op/op1.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-#=================================================
-# DIY script
-# jsjson@163.com 
-#=================================================
-##添加自己的插件库
-sed -i "1isrc-git xiangfeidexiaohuo https://github.com/xiangfeidexiaohuo/openwrt-packages\n" feeds.conf.default
